c23efab3f4c93d2c4341e9c920f5f6fefeeaceef
Ron Howe <ronhowe@hotmail.com>
2021-11-30 11:23:41 -0500

    Added Authorize Test

--- ncrunch/CodeFactory.cs
+++ ncrunch/CodeFactory.cs
@@ -1,9 +1,18 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 
 namespace ncrunch
 {
@@ -31,7 +40,11 @@ namespace ncrunch
             WriteTestHeader("Unauthorized");
 
             using var sut = new SystemUnderTest();
-            using var client = sut.CreateClient();
+            using var client = sut.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri("https://localhost:9999")
+
+            });
             using var response = client.GetAsync("/weatherforecast");
 
             Assert.AreEqual(HttpStatusCode.Unauthorized, response.Result.StatusCode);
@@ -43,13 +56,55 @@ namespace ncrunch
         {
             WriteTestHeader("Authorized");
 
-            Assert.Inconclusive("TODO");
+            using var sut = new SystemUnderTest();
+
+            var client = sut.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddAuthentication("Test")
+                        .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                            "Test", options => { });
+                });
+            })
+            .CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false,
+                BaseAddress = new Uri("https://localhost:9999")
+            });
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Test");
+
+            var response = client.GetAsync("/weatherforecast");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.Result.StatusCode);
         }
 
 
         private static void WriteTestHeader(string tag)
         {
             Trace.WriteLine(String.Format("{0} @ {1}", tag, DateTime.Now));
+        }
+    }
+
+    public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var claims = new[] { new Claim(ClaimTypes.Name, "Test User"), new Claim("scp", "access_as_user") };
+            var identity = new ClaimsIdentity(claims, "Test");
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, "Test");
+
+            var result = AuthenticateResult.Success(ticket);
+
+            return Task.FromResult(result);
         }
     }
 }
