6771002a749244e252e9dc4c112eb086c1805425
Ron Howe <ronhowe@hotmail.com>
2021-12-12 00:13:01 -0500

    Proved WSL and Docker (Still Need Docker Port Mapping)

--- ClassLibrary1/Constants/Constant.cs
+++ ClassLibrary1/Constants/Constant.cs
@@ -4,6 +4,6 @@
     {
         public const string Id = "00000000-0000-0000-0000-000000000000";
         public const string Undefined = "Undefined";
-        public const string ApiEndpoint = "https://localhost:7087";
+        public const string ApiEndpoint = "https://localhost:9999";
     }
 }
